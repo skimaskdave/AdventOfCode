674d8f1e0ce79a732d82b80be40d2cf4793f35f9
skimaskdave <91156462+skimaskdave@users.noreply.github.com>
2021-12-02 08:48:13 +0000

    Day 2
    These are my solutions to day 2

---
+++ Day2/Day2_2/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim fileName As String = "day2.txt"
+        Dim line As String
+        Dim lines As New List(Of String)
+        Dim depth, pos, aim As Integer
+        Using reader As StreamReader = New StreamReader(fileName)
+            Do Until reader.EndOfStream
+                line = reader.ReadLine()
+                lines.Add(line)
+            Loop
+        End Using
+        For i = 0 To lines.Count - 1
+            Dim split() As String = lines(i).Split(" ")
+            If split(0) = "forward" Then
+                pos += Int(split(1))
+                depth += aim * Int(split(1))
+            ElseIf split(0) = "up" Then
+                aim -= Int(split(1))
+            ElseIf split(0) = "down" Then
+                aim += Int(split(1))
+            End If
+        Next
+        Console.WriteLine(depth * pos)
+        Console.ReadKey()
+    End Sub
+
+End Module
